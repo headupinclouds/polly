c20a2514bb0e16c54e2bb15c5fa5011a1bce7d53
David Hirvonen <dhirvonen@elucideye.com>
2016-11-14 17:50:35 -0500

    add ios-10-1-arm64-dep-8-0-hid-sections

--- bin/detail/toolchain_table.py
+++ bin/detail/toolchain_table.py
@@ -239,6 +239,7 @@ if platform.system() == 'Darwin':
   toolchain_table += [
       Toolchain('ios-10-1', 'Xcode', ios_version='10.1'),
       Toolchain('ios-10-1-arm64', 'Xcode', ios_version='10.1'),
+      Toolchain('ios-10-1-arm64-dep-8-0-hid-sections', 'Xcode', ios_version='10.1'),
       Toolchain('ios-10-1-armv7', 'Xcode', ios_version='10.1'),
       Toolchain('ios-10-1-wo-armv7s', 'Xcode', ios_version='10.1'),
       Toolchain('ios-10-0', 'Xcode', ios_version='10.0'),
